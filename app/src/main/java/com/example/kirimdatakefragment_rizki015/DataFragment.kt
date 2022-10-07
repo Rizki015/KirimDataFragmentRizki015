0436f942586232e5398e767e58f4ae5e99ef53da
Your Name <you@example.com>
2022-10-08 01:14:53 +0700

    kirim data fragment

---
+++ app/src/main/java/com/example/kirimdatakefragment_rizki015/DataFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kirimdatakefragment_rizki015
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.*
+import android.view.LayoutInflater
+import android.widget.TextView
+
+
+class DataFragment : Fragment() {
+    override fun onCreateView(
+        inflater : LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_data, container ,false)
+        val tvData = v.findViewById<TextView>(R.id.tvData)
+        if (arguments != null) {
+            val nim = requireArguments().getString("nim")
+            val nama = requireArguments().getString("nama")
+            val prodi = requireArguments().getString("prodi")
+            tvData.text ="""
+                NIM : $nim
+                Nama : $nama
+                Program studi : $prodi
+            """.trimIndent()
+        }
+                return v
+
+    }
+}
